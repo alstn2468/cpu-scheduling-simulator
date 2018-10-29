49ea0d22965719f6fd440c28394e707a6c17ffed
alstn2468 <alstn2468_@naver.com>
2018-10-29 19:39:12 +0900

    Add print Response Time

--- CpuSchedulingAlgorithmsModule/PrintTable.h
+++ CpuSchedulingAlgorithmsModule/PrintTable.h
@@ -11,16 +11,18 @@ void print_table(Process p[], int n)
 {
 	int i;
 
-	puts("+-----+------------+-------------+----------+-------------+--------------+-----------------+");
-	puts("| PID | Burst Time | Arrive Time | Priority | Return Time | Waiting Time | Turnaround Time |");
-	puts("+-----+------------+-------------+----------+-------------+--------------+-----------------+");
+	puts("+-----+------------+-------------+----------+-------------+-----------------+--------------+-----------------+");
+	puts("| PID | Burst Time | Arrive Time | Priority | Return Time |  Response Time  | Waiting Time | Turnaround Time |");
+	puts("+-----+------------+-------------+----------+-------------+-----------------+--------------+-----------------+");
 
 	for (i = 0; i < n; i++) {
-		printf("| %3s |     %3d    |     %3d     |    %3d   |     %3d     |      %3d     |        %3d      |\n",
-			p[i].id, p[i].burst, p[i].arrive_time, p[i].priority, p[i].return_time, p[i].waiting_time, p[i].turnaround_time);
+		printf("| %3s |     %3d    |     %3d     |    %3d   |     %3d     |      %3d        |      %3d     |        %3d      |\n",
+			p[i].id, p[i].burst, p[i].arrive_time, p[i].priority, p[i].return_time, p[i].response_time, p[i].waiting_time, p[i].turnaround_time);
 
-		puts("+-----+------------+-------------+----------+-------------+--------------+-----------------+");
+		puts("+-----+------------+-------------+----------+-------------+-----------------+--------------+-----------------+");
 	}
+
+	puts("\n\n");
 }
 
 #endif
