09c0789c3c82f332fa3e19e127deb64fd78dbb92
alstn2468 <alstn2468_@naver.com>
2018-10-29 18:46:47 +0900

    Add print Return TIme in PrintTable.h

--- CpuSchedulingAlgorithmsModule/PrintTable.h
+++ CpuSchedulingAlgorithmsModule/PrintTable.h
@@ -11,15 +11,15 @@ void print_table(Process p[], int n)
 {
 	int i;
 
-	puts("+-----+------------+-------------+----------+--------------+-----------------+");
-	puts("| PID | Burst Time | Arrive Time | Priority | Waiting Time | Turnaround Time |");
-	puts("+-----+------------+-------------+----------+--------------+-----------------+");
+	puts("+-----+------------+-------------+----------+-------------+--------------+-----------------+");
+	puts("| PID | Burst Time | Arrive Time | Priority | Return Time | Waiting Time | Turnaround Time |");
+	puts("+-----+------------+-------------+----------+-------------+--------------+-----------------+");
 
 	for (i = 0; i < n; i++) {
-		printf("| %3s |     %3d    |     %3d     |    %3d   |      %3d     |        %3d      |\n",
-			p[i].id, p[i].burst, p[i].arrive_time, p[i].priority, p[i].waiting_time, p[i].turnaround_time);
+		printf("| %3s |     %3d    |     %3d     |    %3d   |     %3d     |      %3d     |        %3d      |\n",
+			p[i].id, p[i].burst, p[i].arrive_time, p[i].priority, p[i].return_time, p[i].waiting_time, p[i].turnaround_time);
 
-		puts("+-----+------------+-------------+----------+--------------+-----------------+");
+		puts("+-----+------------+-------------+----------+-------------+--------------+-----------------+");
 	}
 }
 
